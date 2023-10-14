571a453c735fc6c1b1a35471c0c8d3559be9d3bc
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-10-14 12:55:56 -0400

    Binding to Directive properties

--- src/app/directives/renderer-basic-highlight.directive.ts
+++ src/app/directives/renderer-basic-highlight.directive.ts
@@ -1,26 +1,31 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appRendererBasicHighlight]'
 })
 export class RendererBasicHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'yellow';
+
+  @Input() defaultColor: string = 'transparent';
+  @Input('appRendererBasicHighlight') highlightColor: string = 'pink';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
 
   }
   ngOnInit() {
+    this.backgroundColor = 'peach';
     this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = "blue";
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'orange');
-    this.backgroundColor = "pink";
+    this.backgroundColor = this.highlightColor;
 
   }
 }
